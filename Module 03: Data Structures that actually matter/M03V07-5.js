86e6e62ce24561426a97c8240d4e5631dc9994a4
md-zeon <zeon.cse@gmail.com>
2025-11-06 18:07:23 +0600

    feat: Implement Linked List with append() and print() methods, including example usage

---
+++ Module 03: Data Structures that actually matter/M03V07-5.js
@@ -0,0 +1,49 @@
+class Node {
+	constructor(data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	append(data) {
+		const newNode = new Node(data);
+		// If the list is empty
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			// If the list is not empty
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+	}
+
+	print() {
+		const elements = [];
+		let currentNode = this.head;
+
+		while (currentNode) {
+			elements.push(currentNode.data);
+			currentNode = currentNode.next;
+		}
+		elements.push("null");
+		console.log(elements.join(" -> "));
+	}
+}
+
+// Example usage:
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+
+console.log("Linked List contents:");
+list.print(); // Outputs: 10, 20, 30
