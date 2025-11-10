c387dae45b5ce6c8cb126d86ca1d1b12bdd2a64b
md-zeon <zeon.cse@gmail.com>
2025-11-10 21:58:58 +0600

    feat: Add example demonstrating destructuring and array destructuring in TypeScript

---
+++ Module 05: Explore Basic Types of TypeScript/src/M05V09.ts
@@ -0,0 +1,40 @@
+// Destructuring in TypeScript
+const user = {
+	name: {
+		firstName: "Zeanur Rahaman",
+		lastName: "Zeon",
+	},
+	gender: "Male",
+	age: 24,
+	hobbies: ["Reading", "Traveling", "Coding"],
+	favoriteColor: "Blue",
+};
+
+const {
+	name: { firstName, lastName },
+	gender,
+	age,
+	hobbies,
+	favoriteColor,
+}: {
+	name: { firstName: string; lastName: string };
+	gender: string;
+	age: number;
+	hobbies: string[];
+	favoriteColor: string;
+} = user;
+
+console.log(`First Name: ${firstName}`);
+console.log(`Last Name: ${lastName}`);
+console.log(`Gender: ${gender}`);
+console.log(`Age: ${age}`);
+console.log(`Hobbies: ${hobbies.join(", ")}`);
+console.log(`Favorite Color: ${favoriteColor}`);
+
+const friends = ["Alice", "Bob", "Charlie", "David"];
+
+const [firstFriend, secondFriend, ...otherFriends]: string[] = friends;
+
+console.log(`First Friend: ${firstFriend}`);
+console.log(`Second Friend: ${secondFriend}`);
+console.log(`Other Friends: ${otherFriends.join(", ")}`);
